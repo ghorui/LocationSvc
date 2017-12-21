f341b9be1aa11e7b36f58bdd42a3ecf540b12b09
tarun.kumar.ghorui <tarun.kumar.ghorui@accenture.com>
2017-12-21 13:21:55 +0530

    Simple API installation is done

---
+++ LocationSvc/Controllers/LocationController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LocationSvc.Controllers
+{
+    public class LocationController : ApiController
+    {
+        //https://localhost:44373/api/location?cityName=dc
+        public Models.Location GetLocation(string cityName)
+        {
+            return new Models.Location() { latitude = 10, lognitude = 20 };
+        }
+    }
+}
---
+++ LocationSvc/Models/Location.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LocationSvc.Models
+{
+    public class Location
+    {
+        public int latitude { get; set; }
+        public int lognitude { get; set; }
+    }
+}
