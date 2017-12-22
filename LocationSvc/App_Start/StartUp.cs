1c273ce79f5aaebac583363307ef53849d3ac847
tarun.kumar.ghorui <tarun.kumar.ghorui@accenture.com>
2017-12-22 12:07:25 +0530

    updated CORS error - n/w in Chrome

---
+++ LocationSvc/App_Start/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+[assembly: OwinStartup(typeof(LocationSvc.App_Start.StartUp))]
+namespace LocationSvc.App_Start
+{
+    public class StartUp
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+        }
+    }
+}
---
+++ LocationSvcClient/scripts/app.js
@@ -0,0 +1,11 @@
+﻿/// <reference path="lib/angular.js" />
+
+var locationApp = angular.module("locationApp", []);
+
+var locationController = locationApp.controller("locationController", function ($scope, $http) {
+    $scope.getLocation = function () {
+        $http.get("https://localhost:44373/api/location?cityName=dc").then(function (res) {
+            $scope.city = res.data;
+        });
+    }
+});
