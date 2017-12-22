8e15167a8705e5d0f1384bebf9d000b676356ac4
tarun.kumar.ghorui <tarun.kumar.ghorui@accenture.com>
2017-12-22 16:51:42 +0530

    CORS update - https://stackoverflow.com/questions/47937152/microsoft-owin-cors-allow-all-not-working-in-chrome-for-web-api

--- LocationSvc/App_Start/StartUp.cs
+++ LocationSvc/App_Start/StartUp.cs
@@ -4,7 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-
+using System.Web.Http;
 
 [assembly: OwinStartup(typeof(LocationSvc.App_Start.StartUp))]
 namespace LocationSvc.App_Start
@@ -13,7 +13,16 @@ namespace LocationSvc.App_Start
     {
         public void Configuration(IAppBuilder app)
         {
+            //app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
             app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+
+            HttpConfiguration config = new HttpConfiguration();
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+            app.UseWebApi(config);
         }
     }
 }
